35c698264c03436042d3056a134284834620e809
AbdelhakMHD <ae.mehda@gmail.com>
2023-12-10 18:37:05 +0100

    complete Home filters UI

--- components/home/HomeFilters.tsx
+++ components/home/HomeFilters.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+"use client";
+import { HomePageFilters } from "@/constants/filters";
+import React, { useState } from "react";
+import { Button } from "../ui/button";
 
 const HomeFilters = () => {
-  return <div>HomeFilters</div>;
+  const [active, setactive] = useState("newest");
+  // const active = "newest";
+  return (
+    <div className="mt-10 hidden flex-wrap gap-3 md:flex">
+      {HomePageFilters.map((item) => (
+        <Button
+          key={item.value}
+          onClick={() => setactive(item.value)}
+          className={`body-medium rounded-lg px-6 py-3 capitalize shadow-none ${
+            active === item.value
+              ? "bg-primary-100 text-light-500"
+              : "bg-light-800 text-light-500"
+          }`}
+        >
+          {item.name}
+        </Button>
+      ))}
+    </div>
+  );
 };
 
 export default HomeFilters;
