60e5f34f8ec5b1ef272527250d93d166d4a3a533
AbdelhakMHD <ae.mehda@gmail.com>
2024-01-20 18:34:18 +0100

    fix deploy issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { ClerkProvider } from "@clerk/nextjs";
 import React from "react";
-import { Inter, Space_Grotesk } from "next/font/google";
+import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
 import type { Metadata } from "next/types";
 
 import "./globals.css";
@@ -11,7 +11,7 @@ const inter = Inter({
   weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
 });
-const spaceGrotesk = Space_Grotesk({
+const spaceGrotesk = SpaceGrotesk({
   subsets: ["latin"],
   weight: ["300", "400", "500", "600", "700"],
   variable: "--font-spaceGrotesk",
