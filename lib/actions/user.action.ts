43a63c004981e34820df2ad922ea407e503df116
AbdelhakMHD <ae.mehda@gmail.com>
2024-01-20 07:42:24 +0100

    Implement Webhooks and User Actions -delete user-

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectionToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   const { userId } = params;
@@ -10,6 +17,53 @@ export async function getUserById(params: any) {
     const user = await User.findOne({ clerkId: userId });
 
     return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectionToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectionToDatabase();
+    const { clerkId, path, updateData } = params;
+    await User.findByIdAndUpdate({ clerkId }, updateData, { new: true });
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    connectionToDatabase();
+    const { clerkId } = params;
+    const user = await User.findOneAndDelete({ clerkId });
+
+    if (!user) {
+      throw new Error("user not found");
+    }
+
+    // const userQuestionIds = await Question.find({ author: user._id}).distinct('_id');
+
+    // delete user questions
+    await Question.deleteMany({ author: user._id });
+
+    // TODO: delete user answers, comments, etc.
+
+    const deletedUser = await User.findByIdAndDelete(user._id);
+
+    return deletedUser;
   } catch (error) {
     console.log(error);
     throw error;
