49be5302986d5ed7d8a87b41f8c17412c52d55c2
AbdelhakMHD <ae.mehda@gmail.com>
2024-01-26 11:12:38 +0100

    deployment issue

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,7 +4,7 @@ export const connectionToDatabase = async () => {
   mongoose.set("strictQuery", true);
   if (!process.env.MONGODB_URL) return console.log("Missing MongoDB URL");
   if (isConnected) {
-    return console.log("MongoDB is already connected");
+    return console.log("===MongoDB is already connected====");
   }
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
