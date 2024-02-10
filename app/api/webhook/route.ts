76445493cbd1cb370c00c6e5aaa1973e8e1ead59
AbdelhakMHD <ae.mehda@gmail.com>
2024-02-10 22:28:59 +0100

    fixing create user issue

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -60,7 +60,9 @@ export async function POST(req: Request) {
   if (eventType === "user.created") {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
+    console.log("==============Event Data==============");
 
+    console.log(evt.data);
     // Create a new user in your database
     const mongoUser = await createUser({
       clerkId: id,
