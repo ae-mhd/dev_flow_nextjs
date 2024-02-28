d35f83fce26f92033a1a94816b9218201433852e
AbdelhakMHD <ae.mehda@gmail.com>
2024-02-28 12:46:01 +0100

    handle upvote and downvote action

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -1,7 +1,103 @@
-import React from "react";
+"use client";
+import { getQuestionByID, questionUpvote } from "@/lib/actions/question.action";
+import { formatNumber } from "@/lib/utils";
+import Image from "next/image";
 
-const Votes = () => {
-  return <div>Votes</div>;
+interface Props {
+  type: string;
+  itemId: string;
+  userId: string;
+  upvotes: number;
+  hasupVoted: boolean;
+  downvotes: number;
+  hasdownVoted: boolean;
+  hasSaved?: boolean;
+}
+const Votes = ({
+  type,
+  itemId,
+  userId,
+  upvotes,
+  hasupVoted,
+  downvotes,
+  hasdownVoted,
+  hasSaved,
+}: Props) => {
+  const handleVote = async (action: string) => {
+    try {
+      const question = await getQuestionByID({
+        questionId: JSON.parse(itemId),
+      });
+      // await questionUpvote ({});
+      console.log(question);
+      // questionVote({ questionId: itemId });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSave = async () => {};
+  return (
+    <div className="flex gap-5">
+      <div className="flex-center gap-2.5">
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasupVoted
+                ? "/assets/icons/upvoted.svg"
+                : "/assets/icons/upvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="upvote"
+            className="cursor-pointer"
+            onClick={() => handleVote("upvote")}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(upvotes)}
+            </p>
+          </div>
+        </div>
+
+        <div className="flex-center gap-1.5">
+          <Image
+            src={
+              hasdownVoted
+                ? "/assets/icons/downvoted.svg"
+                : "/assets/icons/downvote.svg"
+            }
+            width={18}
+            height={18}
+            alt="downvote"
+            className="cursor-pointer"
+            onClick={() => handleVote("downvote")}
+          />
+
+          <div className="flex-center background-light700_dark400 min-w-[18px] rounded-sm p-1">
+            <p className="subtle-medium text-dark400_light900">
+              {formatNumber(downvotes)}
+            </p>
+          </div>
+        </div>
+      </div>
+
+      {type === "Question" && (
+        <Image
+          src={
+            hasSaved
+              ? "/assets/icons/star-filled.svg"
+              : "/assets/icons/star-red.svg"
+          }
+          width={18}
+          height={18}
+          alt="star"
+          className="cursor-pointer"
+          onClick={handleSave}
+        />
+      )}
+    </div>
+  );
 };
 
 export default Votes;
--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -7,6 +7,7 @@ import {
   CreateQuestionParams,
   GetQuestionByIdParams,
   GetQuestionsParams,
+  QuestionVoteParams,
 } from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
@@ -92,6 +93,55 @@ export async function createQuestion(params: CreateQuestionParams) {
 
     // Increment author's reputation by +5 for creating a question
 
+    revalidatePath(path);
+  } catch (error) {}
+}
+
+export async function questionUpvote(params: QuestionVoteParams) {
+  const { questionId, hasdownVoted, hasupVoted, path, userId } = params;
+  try {
+    await connectionToDatabase();
+    let updateQuery = {};
+    if (hasupVoted) {
+      updateQuery = { $pull: { upvotes: userId } };
+    } else if (hasdownVoted) {
+      updateQuery = {
+        $pull: { downvotes: userId },
+        $push: { upvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { upvote: userId } };
+    }
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+    if (!question) {
+      throw new Error("Question not found");
+    }
+    revalidatePath(path);
+  } catch (error) {}
+}
+export async function questionDownvote(params: QuestionVoteParams) {
+  const { questionId, hasdownVoted, hasupVoted, path, userId } = params;
+  try {
+    await connectionToDatabase();
+    let updateQuery = {};
+    if (hasdownVoted) {
+      updateQuery = { $pull: { downvotes: userId } };
+    } else if (hasupVoted) {
+      updateQuery = {
+        $pull: { upvotes: userId },
+        $push: { downvotes: userId },
+      };
+    } else {
+      updateQuery = { $addToSet: { downvotes: userId } };
+    }
+    const question = await Question.findByIdAndUpdate(questionId, updateQuery, {
+      new: true,
+    });
+    if (!question) {
+      throw new Error("Question not found");
+    }
     revalidatePath(path);
   } catch (error) {}
 }
